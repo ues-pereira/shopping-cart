383b15abd4345dbcc60eb23c80bacd037d451ba7
Ueslei Neves <ueslei.pereira@gmail.com>
2025-01-29 02:13:59 -0300

    add specs

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,19 +5,19 @@ RSpec.describe Product, type: :model do
     it 'validates presence of name' do
       product = described_class.new(price: 100)
       expect(product.valid?).to be_falsey
-      expect(product.errors[:name]).to include("can't be blank")
+      expect(product.errors[:name]).to include("não pode ficar em branco")
     end
 
     it 'validates presence of price' do
       product = described_class.new(name: 'name')
       expect(product.valid?).to be_falsey
-      expect(product.errors[:price]).to include("can't be blank")
+      expect(product.errors[:price]).to include("não pode ficar em branco")
     end
 
     it 'validates numericality of price' do
       product = described_class.new(price: -1)
       expect(product.valid?).to be_falsey
-      expect(product.errors[:price]).to include("must be greater than or equal to 0")
+      expect(product.errors[:price]).to include("deve ser maior ou igual a 0")
     end
   end
 end
